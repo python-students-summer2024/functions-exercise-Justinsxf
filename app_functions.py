05579500c230e49d262a02c98aa95342603bc486
“Justinsxf”.  <XS2456@nyu.edu>
2024-06-11 00:01:50 -0400

    0831

--- app_functions.py
+++ app_functions.py
@@ -14,6 +14,8 @@ def roll_die():
     :returns: the pseudo-random integer.
     """
     # complete this function below here
+    integer = random.randint(1, 6)
+    return integer
 
 
 def get_question_type():
@@ -24,6 +26,13 @@ def get_question_type():
     :returns: "sum" for an addition question, "difference" for a subtraction question.
     """
     # complete this function below here
+    integer = random.randint(1, 6)
+    if integer % 2 == 0:
+        return "sum"
+    else:
+        return "difference"
+   
+
 
 
 def print_question(die_1_value, die_2_value, question_type):
@@ -43,6 +52,17 @@ def print_question(die_1_value, die_2_value, question_type):
     :returns: None
     """
     # complete this function below here
+    
+
+
+    if question_type == "sum":
+        print(f"You rolled a {die_1_value} and a {die_2_value}... What is the sum of {die_1_value} and {die_2_value}?")
+
+    if question_type == "difference":
+        print(f"You rolled a {die_1_value} and a {die_2_value}... What is the difference between {die_1_value} and {die_2_value}?")
+
+
+        
 
 
 def input_answer():
@@ -56,6 +76,13 @@ def input_answer():
     :returns: The user's answer, as an int, if valid; or -1 if the user's response was not valid.
     """
     # complete this function below here
+    response = input("Enter your response: ").strip()
+
+    if response.isdigit() or (response.startswith('-')):
+        return int(response)
+    else:
+        return -1
+    
 
 
 def is_correct_answer(die_1_value, die_2_value, question_type, given_answer):
@@ -69,6 +96,15 @@ def is_correct_answer(die_1_value, die_2_value, question_type, given_answer):
     :returns: True if the user's given answer is correct, False otherwise.
     """
     # complete this function below here
+    if question_type == "sum":
+        correct_answer = die_1_value + die_2_value
+    elif question_type == "difference":
+        correct_answer = abs(die_1_value-die_2_value)
+    else:
+        return False
+    
+    return given_answer == correct_answer
+
 
 
 def print_congratulations(question_type):
@@ -82,6 +118,12 @@ def print_congratulations(question_type):
     :param question_type: A string - either "sum" or "difference" - indicating whether the user was asked to add or subtract the two integers.
     """
     # complete this function below here
+    if question_type == "sum":
+        print("Yes! Congratulations on the successful addition!")
+    elif question_type == "difference":
+        print("Yes! Congratulations on the successful subtraction!")
+
+
 
 
 def print_correct_answer(die_1_value, die_2_value, question_type):
@@ -97,6 +139,12 @@ def print_correct_answer(die_1_value, die_2_value, question_type):
     :param question_type: A string - either "sum" or "difference" - indicating whether the user was asked to add or subtract the two integers.
     """
     # complete this function below here
+    if question_type == "sum":
+        correct_answer = die_1_value + die_2_value
+        print(f"No! The sum of {die_1_value} and {die_2_value} is {correct_answer}!") 
+    elif question_type == "difference":
+        correct_answer = abs(die_1_value-die_2_value)
+        print(f"No! The difference between {die_1_value} and {die_2_value} is {correct_answer}!")
 
 
 def print_error_message():
@@ -107,3 +155,4 @@ def print_error_message():
     - "Sorry - that is an invalid answer.  Bye Bye!"
     """
     # complete this function below here
+    print("Sorry - that is an invalid answer.  Bye Bye!")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """
 An education app that elementary school teachers can use to help teach their students learn addition and subtraction. 
-The app allows students to virtually roll two dice. The values of the dice will be displayed to the students, and the app will then ask students to input either the sum of the two dice or the difference between the two dice. 
+The app allows students to virtually roll two dice. The values of the dice will be displayed to the students, and the app will
+then ask students to input either the sum of the two dice or the difference between the two dice. 
 Students are told whether their answer was correct or not.
 """
 
@@ -23,6 +24,36 @@ def main():
     print("Welcome to the Math App!!!")
     print("")  # line break
     ### write code to complete this function BELOW here ###
+    
+    #Rolls two virtual dice to generate two pseudo-random numbers between 1 and 6, inclusive.
+    dice1 = app_functions.roll_die()
+    dice2 = app_functions.roll_die()
+
+
+    # Pseudo-randomly decide whether the question will be an addition or a subtraction question.
+    question_type = app_functions.get_question_type()
+
+
+
+    # Present the question to the user and ask them to enter their response.
+    app_functions.print_question(dice1, dice2, question_type)
+    user_answer = app_functions.input_answer()
+
+
+
+    # If the user entered an invalid response, print an error message and do nothing further (i.e. do not do the next two steps below).
+    if user_answer == -1:
+        app_functions.print_error_message()
+        return
+
+    # If their answer was correct, congratulate them.
+    if app_functions.is_correct_answer(dice1, dice2, question_type, user_answer):
+        app_functions.print_congratulations(question_type)
+
+
+    # If their answer was incorrect, show them the correct answer.
+    else:
+        app_functions.print_correct_answer(dice1, dice2, question_type)
 
     ### write code to complete this function ABOVE here ###
     print("")  # line break
